16855718c94deebc784c295be116ab34c284eb09
saanvi107 <74966921+saanvi107@users.noreply.github.com>
2020-12-02 16:46:08 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var fixedRect,movingRect;
+
+function setup() {
+  createCanvas(800,400);
+  fixedRect=createSprite(200,200,80,50);
+  movingRect=createSprite(400,200,50,50);
+}
+
+function draw() {
+  background("pink"); 
+  movingRect.x=World.mouseX;
+  movingRect.y=World.mouseY; 
+  console.log(movingRect.x-fixedRect.x);
+  if(movingRect.x-fixedRect.x<fixedRect.width/2+movingRect.width/2 && fixedRect.x-movingRect.x<fixedRect.width/2+movingRect.width/2&& movingRect.y-fixedRect.y<movingRect.height/2+fixedRect.height/2&&fixedRect.y-movingRect.y<fixedRect.height/2+movingRect.height/2    )
+  {
+    movingRect.shapeColor="red";
+    fixedRect.shapeColor="red";
+  }
+  else {
+    movingRect.shapeColor="green";
+    fixedRect.shapeColor="green";
+  }
+  drawSprites();
+}
